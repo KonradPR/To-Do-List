1fa7fe16527f6e39fbab5465bf3db0526d19e969
KonradPR <32310362+KonradPR@users.noreply.github.com>
2017-11-30 18:02:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+$("document").ready(function(){
+    
+    /* button event for showing the div */
+    $("#add").click(function(e){
+       $("#addMenu").removeClass("invisible");
+       $("#addMenu").addClass("visible"); 
+    });
+});
